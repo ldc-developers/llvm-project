69b8ba6638ea68b896e1417f20465989e3c657d7
Martin Kinkelin <noone@nowhere.com>
2019-12-14 13:36:38 +0100

    [LDC] LLD: Avoid parsing `-mllvm*` command-line options if there aren't any
    When invoking the integrated LLD from LDC, it somehow uses a different
    global LLVM command-line parser, one with no registered options, so
    parsing is guaranteed to fail, even if there are no args.
    [It's only needed for LTO codegen options anyway.]

--- lld/COFF/Driver.cpp
+++ lld/COFF/Driver.cpp
@@ -1436,8 +1436,10 @@ void LinkerDriver::linkerMain(ArrayRef<const char *> argsArr) {
     v.push_back(arg->getValue());
     config->mllvmOpts.emplace_back(arg->getValue());
   }
-  cl::ResetAllOptionOccurrences();
-  cl::ParseCommandLineOptions(v.size(), v.data());
+  if (v.size() > 1) {
+    cl::ResetAllOptionOccurrences();
+    cl::ParseCommandLineOptions(v.size(), v.data());
+  }
 
   // Handle /errorlimit early, because error() depends on it.
   if (auto *arg = args.getLastArg(OPT_errorlimit)) {
